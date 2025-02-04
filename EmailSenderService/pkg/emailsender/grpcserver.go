e39d5a0bd2e12be7e4d9ad4b64e56652d3631db4
TkMaxim9 <113044107+TkMaxim9@users.noreply.github.com>
2025-02-04 13:00:10 +0500

    email server

--- EmailSenderService/pkg/emailsender/grpcserver.go
+++ EmailSenderService/pkg/emailsender/grpcserver.go
@@ -4,6 +4,7 @@ import (
 	"EmailSenderService/pkg/api"
 	"EmailSenderService/pkg/services/eventemail"
 	"context"
+	"fmt"
 )
 
 type GRPCServer struct {
@@ -12,6 +13,10 @@ type GRPCServer struct {
 
 func (s *GRPCServer) SendEventNotification(ctx context.Context, req *api.SendEventNotificationRequest) (*api.SendEventNotificationResponse, error) {
 	res, err := eventemail.SendEventNotification(req.GetEmailAddresses(), req.GetEventName(), req.GetEventDate())
+
+	if err != nil {
+		fmt.Println("error")
+	}
 
 	return &api.SendEventNotificationResponse{
 		IsSucceed: res,
--- EmailSenderService/pkg/services/eventemail/service.go
+++ EmailSenderService/pkg/services/eventemail/service.go
@@ -1,26 +1,76 @@
 package eventemail
 
 import (
+	"crypto/tls"
+	"fmt"
 	"net/smtp"
 )
 
 func SendEventNotification(addresses []string, event string, date string) (bool, error) {
 	// Конфигурация SMTP
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
-	from := "m73754896@gmail.com"
-	password := ""
+	smtpHost := "smtp.yandex.ru"
+	smtpPort := "465"
+	from := "mx.tkachenk@yandex.ru"
+	password := "wsoewdncbyqpcpka"
 
-	// Соединение с SMTP сервером
-	auth := smtp.PlainAuth("", from, password, smtpHost)
+	// Настройка соединения с TLS
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: false,
+		ServerName:         smtpHost,
+	}
 
-	to := addresses
-	fullMessageText := "Привет, " + event + " состоится уже " + date + " не забудь!!!"
-	msg := []byte(fullMessageText)
-
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, msg)
+	// Установление соединения с сервером
+	conn, err := tls.Dial("tcp", smtpHost+":"+smtpPort, tlsConfig)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("failed to connect to SMTP server: %v", err)
+	}
+	defer conn.Close()
+
+	// Создание клиента SMTP поверх TLS
+	client, err := smtp.NewClient(conn, smtpHost)
+	if err != nil {
+		return false, fmt.Errorf("failed to create SMTP client: %v", err)
+	}
+
+	// Аутентификация
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+	if err := client.Auth(auth); err != nil {
+		return false, fmt.Errorf("authentication failed: %v", err)
+	}
+
+	// Установка адреса отправителя
+	if err := client.Mail(from); err != nil {
+		return false, fmt.Errorf("failed to set sender: %v", err)
+	}
+
+	// Установка адресатов
+	for _, address := range addresses {
+		if err := client.Rcpt(address); err != nil {
+			return false, fmt.Errorf("failed to set recipient: %v", err)
+		}
+	}
+
+	// Отправка сообщения
+	w, err := client.Data()
+	if err != nil {
+		return false, fmt.Errorf("failed to start data transfer: %v", err)
+	}
+
+	fullMessageText := "Subject: Уведомление о событии\r\n" +
+		"Content-Type: text/plain; charset=\"utf-8\"\r\n\r\n" +
+		"Привет, " + event + " состоится уже " + date + " не забудь!!!"
+
+	if _, err := w.Write([]byte(fullMessageText)); err != nil {
+		return false, fmt.Errorf("failed to write message: %v", err)
+	}
+
+	if err := w.Close(); err != nil {
+		return false, fmt.Errorf("failed to close message writer: %v", err)
+	}
+
+	// Завершение сессии
+	if err := client.Quit(); err != nil {
+		return false, fmt.Errorf("failed to close connection: %v", err)
 	}
 
 	return true, nil
